1bf1858bea2ac93e14dda927ddc725c673ac073a
Nicholas Zafiriou <nicholaszafiriou@gmail.com>
2020-02-11 17:52:32 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -23,7 +23,7 @@ def monopoly_with_third_tier
   # complexity.
   {railroads: {
     pieces: 4,
-    rent_in_dollars: {one_piece_owned: 25, two_pieces_owned: 50, three_pieces_owned: 75, four_pieces_owned: 100},
+    rent_in_dollars: {one_piece_owned: 25, two_pieces_owned: 50, three_pieces_owned: 100, four_pieces_owned: 200},
     names: {a: 0, b: 1, c: 2, d: 3}
       }
   }
